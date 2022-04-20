f5ed6f4e98447d2a992a69f6337ca1355ef6e7f2
Natalie <nlongtin2024@knights.ucf.edu>
2022-04-20 15:17:48 -0400

    Add test cases for creating and editing entries

--- app/src/androidTest/java/com/group24/interactivediary/activities/LoginSignupTest.java
+++ app/src/androidTest/java/com/group24/interactivediary/activities/LoginSignupTest.java
@@ -24,13 +24,16 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LoginSignupAddEntryTest {
+public class LoginSignupTest {
 
     @Rule
     public ActivityTestRule<LoginSignupActivity> mActivityTestRule = new ActivityTestRule<>(LoginSignupActivity.class);
 
     @Test
     public void LoginSignupAddEntryTest() {
+        String testUsername = Helpers.genTestCredential(7);
+        String testPassword = Helpers.genTestCredential(7);
+
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -46,11 +49,11 @@ public class LoginSignupAddEntryTest {
 
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.usernameEditTextSignup), isDisplayed()));
-        appCompatEditText.perform(replaceText("name"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText(testUsername), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.passwordEditTextSignup), isDisplayed()));
-        appCompatEditText2.perform(replaceText("password123"), closeSoftKeyboard());
+        appCompatEditText2.perform(replaceText(testPassword), closeSoftKeyboard());
 
         ViewInteraction materialButton2 = onView(
                 allOf(withId(R.id.signupGoButton), withText("Sign Up"), isDisplayed()));
@@ -101,11 +104,11 @@ public class LoginSignupAddEntryTest {
 
         ViewInteraction appCompatEditText4 = onView(
                 allOf(withId(R.id.usernameEditTextLogin), isDisplayed()));
-        appCompatEditText4.perform(replaceText("name"), closeSoftKeyboard());
+        appCompatEditText4.perform(replaceText(testUsername), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText5 = onView(
                 allOf(withId(R.id.passwordEditTextLogin), isDisplayed()));
-        appCompatEditText5.perform(replaceText("password123"), closeSoftKeyboard());
+        appCompatEditText5.perform(replaceText(testPassword), closeSoftKeyboard());
 
         ViewInteraction materialButton4 = onView(
                 allOf(withId(R.id.loginGoButton), withText("Log In"), isDisplayed()));
@@ -123,72 +126,5 @@ public class LoginSignupAddEntryTest {
         ViewInteraction recyclerView = onView(
                 allOf(withId(R.id.listviewRecyclerView), isDisplayed()));
         recyclerView.check(matches(isDisplayed()));
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(700);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.homeCreateEntryButton), withContentDescription("Create Entry"), isDisplayed()));
-        floatingActionButton.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(700);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        appCompatEditText3 = onView(
-                allOf(withId(R.id.entryCreateTitleEditText), isDisplayed()));
-        appCompatEditText3.perform(replaceText("private entry"), closeSoftKeyboard());
-
-        appCompatEditText4 = onView(
-                allOf(withId(R.id.entryCreateTextEditText), isDisplayed()));
-        appCompatEditText4.perform(replaceText("contents"), closeSoftKeyboard());
-
-        ViewInteraction materialRadioButton = onView(
-                allOf(withId(R.id.createEntryPrivateRadioButton), withText("Private (only you can see and edit this entry)"), isDisplayed()));
-        materialRadioButton.perform(click());
-
-        materialButton3 = onView(
-                allOf(withId(R.id.createEntryPostButton), withText("Post"), isDisplayed()));
-        materialButton3.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(700);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        recyclerView = onView(
-                allOf(withId(R.id.listviewRecyclerView), isDisplayed()));
-        recyclerView.check(matches(isDisplayed()));
-
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.entryTitle), withText("private entry"), isDisplayed()));
-        textView.check(matches(withText("private entry")));
-
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.entryAuthorTextView), withText("name"), isDisplayed()));
-        textView2.check(matches(withText("name")));
-
-        ViewInteraction textView3 = onView(
-                allOf(withId(R.id.entryTimestampTextView), withText("just now"), isDisplayed()));
-        textView3.check(matches(withText("just now")));
-
-        ViewInteraction textView4 = onView(
-                allOf(withId(R.id.entryTextTextView), withText("contents"), isDisplayed()));
-        textView4.check(matches(withText("contents")));
     }
 }
