b385924c56493fb919af91d3607d0923e3111b76
mlin2024 <mlin2024@gmail.com>
2022-04-20 20:52:36 -0400

    Merge

--- app/src/main/java/com/group24/interactivediary/activities/EntryCreateActivity.java
+++ app/src/main/java/com/group24/interactivediary/activities/EntryCreateActivity.java
@@ -33,6 +33,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Pair;
